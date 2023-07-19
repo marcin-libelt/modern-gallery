0c566773351e5addb54eb29023a651215b120dde
Marcin Libelt <marcin.libelt@gmail.com>
2023-07-19 22:43:25 +0200

    modified helpers functions - reformat, naming, comments

--- src/app/lib/helpers.js
+++ src/app/lib/helpers.js
@@ -1,10 +1,16 @@
-export const divideArray = (items, number) => {
-  if (!items.length) {
+/**
+ * Takes a collection and divide it for smallest chunks returning the array of chunks
+ * @param {array} collection
+ * @param {number} chunkSize
+ * @returns {array}
+ */
+export const divideArray = (collection, chunkSize) => {
+  if (!collection.length) {
     return [];
   }
 
-  return items.reduce((accu, current, index) => {
-    const i = Math.floor(index / number);
+  return collection.reduce((accu, current, index) => {
+    const i = Math.floor(index / chunkSize);
     if (accu[i] === undefined) {
       accu.push([]);
     }
