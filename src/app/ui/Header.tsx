cec55d066abc8d97d661f0281019f6c6987191b4
Marcin Libelt <marcin.libelt@gmail.com>
2023-07-29 19:48:07 +0200

    author page and page heading

--- src/app/ui/Header.tsx
+++ src/app/ui/Header.tsx
@@ -14,16 +14,22 @@ export default function Header(): JSX.Element {
     root: "w-full flex sticky items-center bg-black h-24 justify-start gap-3 items-end",
   };
 
+  const HeadigCopy = () => (
+    <>
+      ARCHITECTURE <span className="text-xs">&copy;</span>
+    </>
+  );
+
   return (
     <header className="sticky top-0 z-10">
       <LayoutContainer className={classes.root}>
         {!isHomepage ? (
           <Link href={"/"} className={"text-xl flex no-underline"}>
-            ARCHITECTURE <span className="text-xs">&copy;</span>
+            <HeadigCopy />
           </Link>
         ) : (
           <Heading level={1}>
-            ARCHITECTURE <span className="text-xs">&copy;</span>
+            <HeadigCopy />
           </Heading>
         )}
       </LayoutContainer>
