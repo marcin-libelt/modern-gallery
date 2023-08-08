d1f7427f4750af4e6b470692eb2fceb16dc7bd11
Marcin Libelt <marcin.libelt@gmail.com>
2023-08-08 11:02:42 +0200

    removed redundant div from gallery

--- src/app/ui/RowItem.tsx
+++ src/app/ui/RowItem.tsx
@@ -16,25 +16,24 @@ const RowItem = ({
   const commonCls = `absolute text-xs italic`;
 
   return (
-    <div
-      className={`aspect-square relative group`}
+    <Link
+      href={`/photo/${id}`}
+      className={"aspect-square relative group h-full block"}
       style={{ backgroundColor: color }}
     >
-      <Link href={`/photo/${id}`} className={"h-full block"}>
-        {!loaded && <Loading />}
+      {!loaded && <Loading />}
 
-        <Image
-          src={urls.raw}
-          sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
-          onLoad={() => setLoaded(true)}
-          fill
-          priority
-          alt={alt_description ? alt_description : ""}
-          className={`max-w-full h-auto group-hover:opacity-60 group-hover:scale-[105%]
+      <Image
+        src={urls.raw}
+        sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
+        onLoad={() => setLoaded(true)}
+        fill
+        priority
+        alt={alt_description ? alt_description : ""}
+        className={`max-w-full h-auto group-hover:opacity-60 group-hover:scale-[105%]
               transition-transform`}
-        />
-      </Link>
-    </div>
+      />
+    </Link>
   );
 };
 
