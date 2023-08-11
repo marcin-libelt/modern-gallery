8fc5f76479e7244dad7b20280dce7ebd4bd695f9
Marcin Libelt <marcin.libelt@gmail.com>
2023-08-11 10:11:23 +0200

    addded types for hooks and utils

--- src/app/utils/helpers.ts
+++ src/app/utils/helpers.ts
@@ -1,15 +1,20 @@
+import { number, object } from "prop-types";
+
 /**
  * Takes a collection and divide it for smallest chunks returning the array of chunks
  * @param {array} collection
  * @param {number} chunkSize
  * @returns {array}
  */
-export const divideArray = (collection, chunkSize) => {
+export const divideArray = (
+  collection: Array<object | any>,
+  chunkSize: number
+) => {
   if (!collection.length) {
     return [];
   }
 
-  return collection.reduce((accu, current, index) => {
+  return collection.reduce((accu: Array<object[]>, current, index) => {
     const i = Math.floor(index / chunkSize);
     if (accu[i] === undefined) {
       accu.push([]);
