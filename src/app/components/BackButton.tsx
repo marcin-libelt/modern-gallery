57f72de39dcfc2e8db5c69a83c38f89eba7add7d
Marcin Libelt <marcin.libelt@gmail.com>
2023-08-11 13:28:32 +0200

    fixed back button position on mobile

--- src/app/components/BackButton.tsx
+++ src/app/components/BackButton.tsx
@@ -9,7 +9,7 @@ const BackButton = () => {
     <button
       type="button"
       onClick={() => router.back()}
-      className="text-sm float-right relative -mt-16 z-10"
+      className="text-sm float-right mr-5 fixed right-0 md:mr-0 md:right-auto md:relative -mt-16 z-10"
     >
       &#171; back to the gallery
     </button>
